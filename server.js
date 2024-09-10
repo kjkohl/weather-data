4be2250769e33199918ef930ff90c78d596c4624
kjkohl <kaydenjkohl@gmail.com>
2024-09-10 07:30:46 -0400

    Update server.js

--- server.js
+++ server.js
@@ -8,22 +8,22 @@ const port = 3000;
 app.use(express.static('.'));
 
 app.get('/weather', async (req, res) => {
-    const cityOrZip = req.query.cityOrZip;
+    const city = req.query.city;
     const apiKey = process.env.API_KEY;
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityOrZip}&appid=${apiKey}&units=metric`;
 
     try {
         const response = await fetch(apiUrl);
-        if (!response.ok) {
-            throw new Error('City not found');
-        }
         const data = await response.json();
+        if (data.cod !== 200){
+            return res.status(404).json({ error: 'City not found'});
+        }
         res.json(data);
     } catch (error) {
-        res.status(500).json({ error: 'Error fetching weather data' });
+        res.status(500).json({ error: 'Failed to fetch weather data'});
     }
 });
 
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+    console.log(`Server running on http:\\localhost:${port}`);
 });
